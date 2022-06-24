6e9eaacbd2d2e8bfcdfe95fc9548436128b81bf0
Stephanie Cheuk <scheuk@figma.com>
2022-06-24 10:34:21 -0700

    update UI: shrink footer, remove some text, decrease footer icon size (#9)
    Updates UI elements to better fit the style of figma app.
     - shrinks the footer height
     - shrinks the entire picker height
     - shrinks the footer icon size
     - removes a string when no search results are found
     - removes redundant string name when an emoji is selected
    
    Asana Task: https://app.asana.com/0/0/1202498649164266/f

--- packages/emoji-mart/src/components/Emoji/Emoji.js
+++ packages/emoji-mart/src/components/Emoji/Emoji.js
@@ -15,7 +15,8 @@ export default function Emoji(props) {
   }
 
   emoji || (emoji = SearchIndex.get(id))
-  if (!emoji) return props.fallback
+  if (!emoji || emoji.id === 'female_sign' || emoji.id === 'male_sign')
+    return props.fallback
 
   const emojiSkin = emoji.skins[skin - 1] || emoji.skins[0]
   const src = Images.getUrl(emojiSkin)
@@ -30,9 +31,6 @@ export default function Emoji(props) {
       }}
       alt={emojiSkin.native}
       src={src}
-      onError={({ target }) => {
-        target.style.display = 'none'
-      }}
     />
   )
 }
