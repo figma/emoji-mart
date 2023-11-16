1f42f9f644a2617f7cb3b2e89ab7cf5a49453060
Oliver Wilkie <87673524+oliw-figma@users.noreply.github.com>
2023-11-16 11:07:48 -0800

    emoji-mart: expose NativeSupport tools (#23)
    Expose the NativeSupport helper library so that clients can use its methods to infer what version of emojis are supported

--- packages/emoji-mart/src/index.js
+++ packages/emoji-mart/src/index.js
@@ -1,5 +1,5 @@
 export { PickerElement as Picker } from './components/Picker'
 export { EmojiElement as Emoji } from './components/Emoji'
 
-export { SearchIndex, Store } from './helpers'
+export { SearchIndex, Store, NativeSupport } from './helpers'
 export { init, Data, I18n } from './config'
