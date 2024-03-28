703b22e20b073d35499affeb8b17450319987433
Nate Gainsboro <110426631+ngainsboro-figma@users.noreply.github.com>
2024-03-28 14:31:27 -0700

    Emoji: Add comments about no-longer-in-use code path. (#25)

--- packages/emoji-mart/src/helpers/images.js
+++ packages/emoji-mart/src/helpers/images.js
@@ -1,3 +1,6 @@
+// At writing, this URL is not in use. If it is used again,
+// it will need to be updated appropriately to the most
+// recent figma-hosted emoji URL base.
 const URL = 'https://static.figma.com/emoji/4/64/'
 
 function getUrl(emojiSkin) {
