7ae50cb77a04aa2f17a66d8b1463ef88aef733b3
Justin Ye <jye@figma.com>
2025-07-09 13:07:03 -0400

    add labels for skin tone btn

--- packages/emoji-mart/src/components/Picker/Picker.js
+++ packages/emoji-mart/src/components/Picker/Picker.js
@@ -814,13 +814,16 @@ export default class Picker extends Component {
           ref={this.refs.skinToneButton}
           class="skin-tone-button flex flex-auto flex-center flex-middle"
           aria-selected={this.state.showSkins ? '' : undefined}
-          aria-label={I18n.skins.choose}
+          aria-label={`${I18n.skins.choose}, ${I18n.skins[this.state.skin]}`}
           title={I18n.skins.choose}
           onClick={this.openSkins}
           style={{
             width: this.props.emojiSize,
             height: this.props.emojiSize,
           }}
+          aria-controls={'skin-tone-selector'}
+          aria-haspopup={true}
+          aria-expanded={this.state.showSkins}
         >
           <span class={`skin-tone skin-tone-${this.state.skin}`}></span>
         </button>
@@ -848,6 +851,7 @@ export default class Picker extends Component {
       <div
         ref={this.refs.menu}
         role="radiogroup"
+        id={'skin-tone-selector'}
         aria-label={I18n.skins.choose}
         class="menu hidden"
         data-position={position.top ? 'top' : 'bottom'}
