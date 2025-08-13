cf5b52b22efaffd0f51fbb9c5a43b9b05eae0062
Julia Wright <julia.wright@figma.com>
2025-08-13 14:54:16 -0600

    fix emoji image URL

--- packages/emoji-mart/src/helpers/images.js
+++ packages/emoji-mart/src/helpers/images.js
@@ -1,7 +1,7 @@
 // At writing, this URL is not in use. If it is used again,
 // it will need to be updated appropriately to the most
 // recent figma-hosted emoji URL base.
-const URL = 'https://static.figma.com/emoji/4/noto/small/'
+const URL = 'https://static.figma.com/emoji/5/noto/small/'
 
 function getUrl(emojiSkin) {
   return URL + emojiSkin.unified?.toLowerCase() + '.png'
