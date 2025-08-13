34b7a1bd2620f66fdc2d08ec81177a8d275428f5
Julia Wright <julia.wright@figma.com>
2025-08-13 16:24:26 -0600

    update props.set when new element is created

--- packages/emoji-mart/src/components/Picker/PickerElement.js
+++ packages/emoji-mart/src/components/Picker/PickerElement.js
@@ -19,6 +19,7 @@ export default class PickerElement extends ShadowElement {
       onEmojiSelect,
       onClickOutside,
       onEscapeKeydown,
+      set: this.props.set ?? pickerProps.set,
     }
 
     render(<Picker {...props} />, this.shadowRoot)
